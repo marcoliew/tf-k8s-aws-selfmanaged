a545eb1f954fae39c7cb8533d9f78afb9ea2f8ca
marcoliew <60585553+marcoliew@users.noreply.github.com>
2024-10-27 10:25:30 +1100

    setup dynamic backend

--- 1-provider.tf
+++ 1-provider.tf
@@ -8,15 +8,15 @@ provider "aws" {
 terraform {
   required_version = ">= 1.0"
 
-  backend "remote" {
-    # The name of your Terraform Cloud organization.
-    organization = "xeniumsolution"
+  # backend "remote" {
+  #   # The name of your Terraform Cloud organization.
+  #   organization = "xeniumsolution"
 
-    # The name of the Terraform Cloud workspace to store Terraform state files in.
-    workspaces {
-      name = "tf-k8s-aws-selfmanaged"
-    }
-  }
+  #   # The name of the Terraform Cloud workspace to store Terraform state files in.
+  #   workspaces {
+  #     name = "tf-k8s-aws-selfmanaged"
+  #   }
+  # }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
